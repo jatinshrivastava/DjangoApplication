76282d94c2a365b3eb9816d3fe35b899c60745f8
Jatin Shrivastava <31873847+jatinsrivastava@users.noreply.github.com>
2019-07-02 13:23:25 +0530

    Update views.py

--- uploadmethod/views.py
+++ uploadmethod/views.py
@@ -10,19 +10,23 @@ from TextToPPTLibrary.texttoppt.orchestrator import TextToPPTOrchestrator
 # Create your views here.
 
 def uploadtxtfiles(request):
-       if request.method == 'POST':
-       	doc = request.FILES['document']
-       	filestorage = FileSystemStorage()
-       	filename = filestorage.save(doc.name,doc)
-       	name = filename+".pptx"
-       	file_path = os.path.join(settings.MEDIA_ROOT,name)
-       	in_path = os.path.join(settings.MEDIA_ROOT,filename)
-       	out_path = os.path.join(settings.MEDIA_ROOT,filename+".pptx") #combine the directory with file
-       	TextToPPTLibrary_class = TextToPPTOrchestrator()
-       	TextToPPTLibrary_class.ConvertTextFileToPPT(in_path,out_path)
-       	if os.path.exists(file_path):
-       		with open(file_path,'rb') as file:
-       			response = HttpResponse(file.read(),content_type = "application/vnd.openxmlformats-officedocument.presentationml.presentation")
-       			response['Content-Disposition'] = 'inline;filename=' + os.path.basename(file_path)
-       			return response
-       return render(request,'uploadmethod/upload.html')
+    if request.method == 'POST':
+		# create a form instance and populate it with data from the request:
+        form = uploadFileForm(request.POST, request.FILES)
+        doc = request.FILES['document']
+        shape = form.data['shapeType']
+        filestorage = FileSystemStorage()
+        filename = filestorage.save(doc.name,doc)
+        name = filename+".pptx"
+        file_path = os.path.join(settings.MEDIA_ROOT,name)
+        in_path = os.path.join(settings.MEDIA_ROOT,filename)
+        out_path = os.path.join(settings.MEDIA_ROOT,filename+".pptx") #combine the directory with file
+        TextToPPTLibrary_class = TextToPPTOrchestrator()
+        TextToPPTLibrary_class.SetShapeType(shape)
+        TextToPPTLibrary_class.ConvertTextFileToPPT(in_path,out_path)
+        if os.path.exists(file_path):
+            with open(file_path,'rb') as file:
+                response = HttpResponse(file.read(),content_type = "application/vnd.openxmlformats-officedocument.presentationml.presentation")
+                response['Content-Disposition'] = 'inline;filename=' + os.path.basename(file_path)
+                return response
+    return render(request,'uploadmethod/upload.html')
